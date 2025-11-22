303bb65e6d1cec139a417e70e31b4278ce36dc4b
David Heidari <davidheidari2@gmail.com>
2025-11-22 15:04:12 +0100

    add back to top button

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const menuToggle = document.querySelector('.menu-toggle')
   const closeMenuToggle = document.querySelector('.close-menu-toggle') // Vår nya knapp
   const mainNav = document.getElementById('main-nav')
+  const backToTopButton = document.getElementById('back-to-top')
 
+  // Visa eller dölj "Tillbaka till toppen"-knappen baserat på scrollposition
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = 'flex';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+
+  backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+  // Lägg till eventlyssnare för hamburgarmenyn
   menuToggle.addEventListener('click', () => {
     mainNav.classList.toggle('active')
     menuToggle.setAttribute('aria-expanded', mainNav.classList.contains('active'))
