7be3ece3bb7fcc398574b4caf9001ac879f610c4
David Heidari <davidheidari2@gmail.com>
2025-11-24 15:24:04 +0100

    add dark light theme

--- script.js
+++ script.js
@@ -4,6 +4,44 @@ document.addEventListener('DOMContentLoaded', () => {
   const mainNav = document.getElementById('main-nav')
   const backToTopButton = document.getElementById('back-to-top')
 
+  // Initialisera temainställningar
+  const themeToggle = document.getElementById('theme-toggle')
+  const body = document.body
+  const storageKey = 'preferredTheme'
+
+  // Funktion för att tillämpa temat
+  const setTheme = (theme) => {
+    if (theme === 'dark') {
+      body.classList.add('dark-mode')
+      // uppdatera aria-label för tillgänglighet
+      themeToggle.setAttribute('aria-label', 'Byt till ljust tema')
+      localStorage.setItem(storageKey, 'dark')
+
+    } else {
+      body.classList.remove('dark-mode')
+      // uppdatera aria-label för tillgänglighet
+      themeToggle.setAttribute('aria-label', 'Byt till mörkt tema')
+      localStorage.setItem(storageKey, 'light')
+    }
+  }
+
+  // initialesera temat baserat på lagring
+  const savedTheme = localStorage.getItem(storageKey);
+  // använd systemetsinställning (prefers-color-scheme) som fallback
+  const initialTheme = savedTheme || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+    setTheme(initialTheme);
+
+  // Lägg till eventlyssnare för temaväxling
+  if(themeToggle){
+    themeToggle.addEventListener('click', () => {
+      //konrollera det nuvarande temat baserat på klassen
+      const isDarkMode = body.classList.contains('dark-mode')
+      const newTheme = isDarkMode ? 'light' : 'dark'
+
+      setTheme(newTheme)
+    })
+  }
+    
   // Visa eller dölj "Tillbaka till toppen"-knappen baserat på scrollposition
   window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
@@ -12,6 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
       backToTopButton.style.display = 'none';
     }
   });
+
 
   backToTopButton.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -36,6 +75,12 @@ document.addEventListener('DOMContentLoaded', () => {
       menuToggle.setAttribute('aria-expanded', 'false')
     })
   })
+
+
+
+
+
+
 })
 
 
